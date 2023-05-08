e742515132722481cfbbb5ac7a680fa55d3033c1
VLiras <valentinliras@gmail.com>
2023-05-08 16:23:10 -0300

    Creating Password generator

---
+++ Pass_generetor/Passwords.py
@@ -0,0 +1,5 @@
+assets = [0,1,2,3,4,5,6,7,8,9]
+vocals = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for i in range(len(vocals)):
+    vocals[i] = vocals[i].lower()
+print(vocals)
