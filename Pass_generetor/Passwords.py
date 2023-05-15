31222cd1707cef2df2f45d1dd32432cbc146c6f9
VLiras <valentinliras@gmail.com>
2023-05-15 13:22:50 -0300

    Fixing arrays

--- Pass_generetor/Passwords.py
+++ Pass_generetor/Passwords.py
@@ -1,22 +1,23 @@
 import random
 numbers = [0,1,2,3,4,5,6,7,8,9]
-vocals = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-for i in range(len(vocals)):
-    vocals[i] = vocals[i].lower()
-# print(vocals)
+abecedary = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+lowerAbc = [i.lower() for i in abecedary]
 def getRandom (min, max):
     ans = random.randint(min, max)
     return ans
     
-password = []
-limit = int(input('Ingrese los caracteres maximos de nu nueva contraseña: '))
+subPassword = []
 def getPassword ():
+    limit = int(input('Ingrese los caracteres maximos de nu nueva contraseña: '))
+    space = ''
     for i in range(0, limit):
-        value = getRandom(0,len(vocals))
-        pass_letters = vocals[value]
-        password.append(pass_letters)
-        # print(value)
+        letters = getRandom(0,len(abecedary))
+        pass_Upper = abecedary[letters]
+        subPassword.append(pass_Upper)
+    password = space.join(subPassword)
+    return print(password)
 getPassword()
-print(password)
+
 
 
