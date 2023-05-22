322e820b303fa156e49e3d30031fabbfef0ee3dc
VLiras <valentinliras@gmail.com>
2023-05-22 13:04:56 -0300

    Reto progamacion: Buscador Viernes 13

--- Friday 13/Friday.py
+++ Friday 13/Friday.py
@@ -1,7 +1,6 @@
 import datetime
 
-
-# print(date.strftime('%A')) # %A => Nombre del dia
+# print(date.strftime('%A')) => Nombre del dia
 # print(date.strftime('%B')) => Numero de dia del mes
 
 def searchFriday_13(month, year):
@@ -10,11 +9,12 @@ def searchFriday_13(month, year):
         print('Si, este mes tendras un viernes 13')
     else: print('Este mes no tiene un viernes 13')
 
-def setMonth():
-    year = int(input('Ingrese el año: '))
-    month = int(input('Ingrese el mes: '))
-    searchFriday_13(month, year)
-setMonth()
+while True:
+    year = input('Ingrese el año: ')
+    if (year == 'q' or year == 'quit') : break
+    month = input('Ingrese el mes: ')
+    searchFriday_13(int(month), int(year))
+
 
    
 
